ef629487d2aee31b350923dbd2f2a201ecdfff0b
Luis-Lindner <luislondner29@gmail.com>
2025-04-21 11:21:33 -0300

    commit inicial novo repositório

---
+++ src/main/java/com/anatonelly/freteexpress/FreteexpressApplication.java
@@ -0,0 +1,13 @@
+package com.anatonelly.freteexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anatonelly/freteexpress/FreteexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anatonelly.freteexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
