3fc43551c9818b7026cac7a4fd8f322ce886e435
mauriciossf05 <162533152+mauriciossf05@users.noreply.github.com>
2025-05-10 20:22:33 -0300

    Update Veiculo.java
    Adicionado Getters e Setters para: numeroRenavam, especificacoes, altura, comprimento, largura, quantidadeEixos, possuiLona, categoria e tipoCarroceria.

--- src/main/java/com/anatonelly/freteexpress/model/Veiculo.java
+++ src/main/java/com/anatonelly/freteexpress/model/Veiculo.java
@@ -1,10 +1,8 @@
 package com.anatonelly.freteexpress.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class Veiculo {
 
     @Id
@@ -24,4 +22,94 @@ public class Veiculo {
     @OneToOne
     @JoinColumn(name = "motorista_id")
     private Motorista motorista;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroRenavam() {
+        return numeroRenavam;
+    }
+
+    public void setNumeroRenavam(String numeroRenavam) {
+        this.numeroRenavam = numeroRenavam;
+    }
+
+    public String getEspecificacoes() {
+        return especificacoes;
+    }
+
+    public void setEspecificacoes(String especificacoes) {
+        this.especificacoes = especificacoes;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(Double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public Double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    public Integer getQuantidadeEixos() {
+        return quantidadeEixos;
+    }
+
+    public void setQuantidadeEixos(Integer quantidadeEixos) {
+        this.quantidadeEixos = quantidadeEixos;
+    }
+
+    public Boolean getPossuiLona() {
+        return possuiLona;
+    }
+
+    public void setPossuiLona(Boolean possuiLona) {
+        this.possuiLona = possuiLona;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipoCarroceria() {
+        return tipoCarroceria;
+    }
+
+    public void setTipoCarroceria(String tipoCarroceria) {
+        this.tipoCarroceria = tipoCarroceria;
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
 }
+
