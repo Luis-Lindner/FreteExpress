ee07315d8f3da375ff9f7e3e7a98499c4af0ddc3
mauriciossf05 <162533152+mauriciossf05@users.noreply.github.com>
2025-05-10 20:20:16 -0300

    Update Motorista.java
    Adicionado Getters e Setters para: email, senha, nomeCompleto, cpf, enderecoCompleto, celular, cnh, numeroAntt e imagemPerfil.

--- src/main/java/com/anatonelly/freteexpress/model/Motorista.java
+++ src/main/java/com/anatonelly/freteexpress/model/Motorista.java
@@ -1,10 +1,8 @@
 package com.anatonelly.freteexpress.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class Motorista {
 
     @Id
@@ -23,4 +21,94 @@ public class Motorista {
 
     @OneToOne(mappedBy = "motorista", cascade = CascadeType.ALL)
     private Veiculo veiculo;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEnderecoCompleto() {
+        return enderecoCompleto;
+    }
+
+    public void setEnderecoCompleto(String enderecoCompleto) {
+        this.enderecoCompleto = enderecoCompleto;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getNumeroAntt() {
+        return numeroAntt;
+    }
+
+    public void setNumeroAntt(String numeroAntt) {
+        this.numeroAntt = numeroAntt;
+    }
+
+    public byte[] getImagemPerfil() {
+        return imagemPerfil;
+    }
+
+    public void setImagemPerfil(byte[] imagemPerfil) {
+        this.imagemPerfil = imagemPerfil;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
 }
+
