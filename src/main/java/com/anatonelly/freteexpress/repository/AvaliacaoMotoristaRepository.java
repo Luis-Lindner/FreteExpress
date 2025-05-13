3babc08d7356d03c48b97c93d325bd7319b74503
caueribasapolinario <caueapolinariogold@gmail.com>
2025-05-13 00:05:04 -0300

    feat(model): adicionar entidades AvaliacaoMotorista, EmpresaCliente e UsuarioAdministrador com seus relacionamentos

---
+++ src/main/java/com/anatonelly/freteexpress/repository/AvaliacaoMotoristaRepository.java
@@ -0,0 +1,7 @@
+package com.anatonelly.freteexpress.repository;
+
+import com.anatonelly.freteexpress.model.AvaliacaoMotorista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoMotoristaRepository extends JpaRepository<AvaliacaoMotorista, Long> {
+}
